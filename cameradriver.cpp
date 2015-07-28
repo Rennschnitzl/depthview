b6b776dead41cf301dd2b8cbe436a032b4201ffa
Rennschnitzl <pitter.flock@googlemail.com>
2015-07-28 20:02:52 +0200

    test

---
+++ cameradriver.cpp
@@ -0,0 +1,7 @@
+#include "cameradriver.h"
+
+CameraDriver::CameraDriver()
+{
+
+}
+
---
+++ cameradriver.h
@@ -0,0 +1,11 @@
+#ifndef CAMERADRIVER_H
+#define CAMERADRIVER_H
+
+
+class CameraDriver
+{
+public:
+    CameraDriver();
+};
+
+#endif // CAMERADRIVER_H
