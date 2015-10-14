613f611d561aaa4973b7b477158988f4fa850e11
Rennschnitzl <pitter.flock@googlemail.com>
2015-10-15 01:23:46 +0200

    adding colormapping to display adding averaging over window, taking zero-measurements into account

--- main.cpp
+++ main.cpp
@@ -36,13 +36,17 @@ int main()
     cv::Mat rgbimage, irimage, depthimage;
 
     //namedWindow( "rgb window", WINDOW_AUTOSIZE );
-    namedWindow( "depth window", WINDOW_AUTOSIZE );
-    namedWindow( "ir window", WINDOW_AUTOSIZE );
+    namedWindow( "depth", WINDOW_AUTOSIZE );
+    namedWindow( "ir", WINDOW_AUTOSIZE );
     namedWindow( "ir averages", WINDOW_AUTOSIZE);
+    namedWindow( "depth averages", WINDOW_AUTOSIZE);
+    namedWindow( "zeroes", WINDOW_AUTOSIZE);
     int key;
 
     std::vector<cv::Mat> irlist, depthlist;
-    cv::Mat result(480, 640, CV_8U, Scalar::all(0));
+    cv::Mat result_ir(480, 640, CV_8U, Scalar::all(0));
+    cv::Mat result_zeroes(480, 640, CV_8U, Scalar::all(0));
+    cv::Mat result_depth(480, 640, CV_16U, Scalar::all(0));
 
     while(true)
     {
@@ -57,7 +61,7 @@ int main()
         irlist.clear();
         depthlist.clear();
         // grab image vector
-        for(int i = 0; i<20; i++)
+        for(int i = 0; i<40; i++)
         {
             nanosleep(&slptm,NULL);
             //colorcam->updateData(&rgbimage);
@@ -66,18 +70,26 @@ int main()
             depthlist.push_back(depthimage.clone());
         }
 
-        std::cout << irlist[0].rows << " - " << irlist[0].cols << std::endl;
 
         for (int i = 0; i < irlist[0].rows; i++)
         {
             for (int j = 0; j < irlist[0].cols; j++)
             {
-                int tempresult = 0;
+                int tempresult_ir = 0;
+                int tempresult_depth = 0;
+                int depth_zeroes = 0;
                 for(int il = 0; il < irlist.size(); il++) {
-                    //std::cout << i << " " << j << " " << il << std::endl;
-                    tempresult += (int)irlist[il].at<uchar>(i,j);
+                    tempresult_ir += (int)irlist[il].at<uchar>(i,j);
+                    tempresult_depth += (int)depthlist[il].at<ushort>(i,j);
+                    if((int)depthlist[il].at<ushort>(i,j) == 0)
+                        depth_zeroes++;
                 }
-                result.at<uchar>(i,j) = tempresult/irlist.size();
+                result_ir.at<uchar>(i,j) = tempresult_ir/irlist.size();
+                if(depthlist.size()-depth_zeroes != 0)
+                    result_depth.at<ushort>(i,j) = tempresult_depth/(depthlist.size()-depth_zeroes);
+                else
+                    result_depth.at<ushort>(i,j) = 0;
+                result_zeroes.at<uchar>(i,j) = (int)((240/irlist.size())*depth_zeroes);
             }
         }
 
@@ -86,10 +98,31 @@ int main()
         std::cout << "Distance in center: " << (int)depthimage.at<ushort>(240,320) << std::endl;
         depthimage.at<ushort>(240,320) = 65000;
 
+
+        // colormap
+        // convert for colormapping
+        Mat depth_cv_8_avg, depth_cv_8;
+        result_depth.convertTo(depth_cv_8_avg,CV_8U,1.0/256.0);
+        depthimage.convertTo(depth_cv_8,CV_8U,1.0/256.0);
+
+        Mat cm_depth_avg, cm_depth;
+        applyColorMap(depth_cv_8_avg, cm_depth_avg, COLORMAP_HSV  );
+        applyColorMap(depth_cv_8, cm_depth, COLORMAP_HSV  );
+
         //imshow( "rgb window", rgbimage );
-        imshow( "depth window", depthimage );
-        imshow( "ir window", irlist[0] );
-        imshow( "ir averages", result);
+
+        // show depth related
+        imshow( "depth", cm_depth );
+        imshow( "depth averages", cm_depth_avg);
+//        imshow( "depth", depthimage );
+//        imshow( "depth averages", result_depth);
+        imshow( "zeroes", result_zeroes);
+
+        // show ir related
+        imshow( "ir", irlist[0] );
+        imshow( "ir averages", result_ir);
+
+
 
 
 
